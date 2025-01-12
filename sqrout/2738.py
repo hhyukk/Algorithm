34b665d68b9189d3c1139311c803b719fef5430a
hhyukk <hhyukk1273@gmail.com>
2025-01-12 17:25:20 +0900

    새싹 - 배열, 2738 행렬 덧셈 (PLUS)

--- sqrout/2738.py
+++ sqrout/2738.py
@@ -14,3 +14,15 @@ for i in range(n):
     for j in range(m):
         print(a[i][j]+b[i][j], end=" ")
     print()
+    
+# GPT Version
+# N, M = map(int, input().split())
+
+# A = [list(map(int, input().split())) for _ in range(N)]
+
+# B = [list(map(int, input().split())) for _ in range(N)]
+
+# result = [[A[i][j] + B[i][j] for j in range(M)] for i in range(N)]
+
+# for row in result:
+#     print(row)
