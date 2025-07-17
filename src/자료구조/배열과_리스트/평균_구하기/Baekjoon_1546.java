f630f45b57982ad4ad7721d07c7fa2e5c6619e05
hhyukk <Hhyukk1273@gmail.com>
2025-07-17 18:38:26 +0900

    자료구조 - 배열과 리스트

---
+++ src/자료구조/배열과_리스트/평균_구하기/Baekjoon_1546.java
@@ -0,0 +1,30 @@
+package 자료구조.배열과_리스트.평균_구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int countN = Integer.parseInt(br.readLine());
+
+        double[] scores = new double[countN];
+        double maxScore = 0.0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < countN; i++) {
+            scores[i] = Double.parseDouble(st.nextToken());
+            maxScore = Math.max(maxScore, scores[i]);
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < countN; i++) {
+            sum += (scores[i] / maxScore) * 100;
+        }
+
+        double average = sum / countN;
+        System.out.println(average);
+    }
+}
