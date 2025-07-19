a250eeec0c3c3b4694ba8efac8eea9aa47eead17
hhyukk <hhyukk1273@gmail.com>
2025-07-19 14:50:34 +0900

    백준 실버4 1940번

---
+++ src/자료구조/투_포인터/주몽의_명령/Baekjoon_1940.java
@@ -0,0 +1,34 @@
+package 자료구조.투_포인터.주몽의_명령;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1940 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 재료의 개수
+        int M = Integer.parseInt(br.readLine()); // 원하는 수
+        int count = 0;
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 이중 for문으로 모든 쌍 검사
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] + arr[j] == M) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
