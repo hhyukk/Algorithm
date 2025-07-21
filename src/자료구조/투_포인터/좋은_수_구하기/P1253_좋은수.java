c038e63eb3dcda110726d0bb248c2180fad2bdb6
hhyukk <hhyukk1273@gmail.com>
2025-07-21 13:16:19 +0900

    백준 골드4 1253번 리팩토링

--- src/자료구조/투_포인터/좋은_수_구하기/P1253_좋은수.java
+++ src/자료구조/투_포인터/좋은_수_구하기/P1253_좋은수.java
@@ -10,30 +10,45 @@ public class P1253_좋은수 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
+        int N = Integer.parseInt(br.readLine()); // 수의 개수
+        int count = 0; // 좋은 수 개수
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 수 배열 입력 받기
         int[] array = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
+
+        // 정렬 (투 포인터 사용을 위함)
         Arrays.sort(array);
 
+        // 각 수가 좋은 수인지 판단
         for (int i = 0; i < N; i++) {
-            int target = array[i];
+            int target = array[i]; // 현재 검사할 수
             int start = 0;
             int end = N - 1;
+
             while (start < end) {
-                if (array[start] + array[end] > array[i]) {
-                    end--;
-                } else if (array[start] + array[end] == array[i]) {
-                    if (start != i && end != i) {
-                        count++;
-                        break;
-                    }
-                } else if (array[start] + array[end] < array[i]) {
+                // 자기 자신은 포함하면 안 됨
+                if (start == i) {
                     start++;
+                    continue;
+                }
+                if (end == i) {
+                    end--;
+                    continue;
+                }
+
+                int sum = array[start] + array[end];
+
+                if (sum == target) {
+                    count++; // 좋은 수 발견
+                    break;
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    end--;
                 }
             }
         }
