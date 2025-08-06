1f284d515ed90a83a43a44aab3b980aaecede5b0
hhyukk <hhyukk1273@gmail.com>
2025-08-06 23:31:47 +0900

    백준 브론즈2 - 2750번: 수 정렬하기

---
+++ src/정렬/버블_정렬/Baekjoon_2750.java
@@ -0,0 +1,38 @@
+package 정렬.버블_정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 버블 정렬
+        for (int i = 0; i < N - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < N - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break; // 정렬 완료 시 조기 종료
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int value : arr) {
+            sb.append(value + "\n");
+        }
+        System.out.println(sb);
+    }
+}
