5746ec01a9e0e8dbdd979c9637b857c5bb386f8e
hhyukk <hhyukk1273@gmail.com>
2025-08-10 10:21:58 +0900

    백준 실버5 - 1427번: 소트인사이드
    문자열, 정렬

---
+++ src/정렬/선택_정렬/Baekjoon_1427.java
@@ -0,0 +1,40 @@
+package 정렬.선택_정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String numberStr = br.readLine();
+        int[] digits = new int[numberStr.length()];
+
+        for (int i = 0; i < digits.length; i++) {
+            digits[i] = numberStr.charAt(i) - '0';
+        }
+
+        // 선택 정렬: 가장 큰 수를 앞으로 보내기
+        for (int i = 0; i < digits.length - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < digits.length; j++) {
+                if (digits[maxIndex] < digits[j]) {
+                    maxIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = digits[i];
+            digits[i] = digits[maxIndex];
+            digits[maxIndex] = temp;
+        }
+
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int digit : digits) {
+            sb.append(digit);
+        }
+        System.out.println(sb);
+    }
+}
