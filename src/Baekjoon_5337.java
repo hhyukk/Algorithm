ef58c8f39166ee8c8e6bf14971a163d3e442a2fd
hhyukk <hhyukk1273@gmail.com>
2025-08-10 10:21:13 +0900

    백준 브론즈5 - 5337번: 웰컴
    구현

---
+++ src/Baekjoon_5337.java
@@ -0,0 +1,7 @@
+public class Baekjoon_5337 {
+    public static void main(String[] args) {
+        System.out.print(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
